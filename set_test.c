96c7977a478c7b21e15cc8f50489d13e30a89776
krantup <progpabsanch@gmail>
2018-03-05 16:48:04 +0100

    Fixed debug file

--- set_test.c
+++ set_test.c
@@ -9,7 +9,7 @@ int main()
 	int i, inv_size=15;
 	Id j=2;
 
-	test_set = set_create(inv_size); 
+	test_set = set_create(inv_size);
 
 	if(!test_set) goto error0;
 
@@ -18,18 +18,18 @@ int main()
 		if(set_add(test_set, j)==ERROR) goto error1;
 		j++;
 	}
-	
+
 	if(set_print_debug(stdout, test_set)==ERROR) goto error1;
-	
+
 	test_set_copy = set_cp_all(test_set);
 
 	printf("\n################ Now we remove a couple of items\n\n");
 
-	if(set_pick(test_set, 3)==ERROR) goto error1;
-	if(set_pick(test_set, 7)==ERROR) goto error1;
-	if(set_pick(test_set, 1)==ERROR) goto error1;
-	if(set_pick(test_set, 8)==ERROR) goto error1;
-	
+	if(set_del(test_set, 3)==ERROR) goto error1;
+	if(set_del(test_set, 7)==ERROR) goto error1;
+	if(set_del(test_set, 2)==ERROR) goto error1;
+	if(set_del(test_set, 8)==ERROR) goto error1;
+
 	if(set_print_debug(stdout, test_set)==ERROR) goto error1;
 
 	printf("\n################ Now we rearrange\n\n");
@@ -43,11 +43,13 @@ int main()
 	if(set_rm_all(test_set)==ERROR) goto error1;
 
 	if(set_print_debug(stdout, test_set)==ERROR) goto error1;
-	
+
 	printf("\n################ Now we print test copy\n\n");
 
 	if(set_print_debug(stdout, test_set_copy)==ERROR) goto error1;
-	
+
+
+	printf("\nEverything works as intended, lets go!\n\n");
 	return 0;
 
 	error1:
