595f6501d4c795314b35c5c00c50acfa75e2470b
Guillermo Rios <guilleerioss@gmail.com>
2018-03-19 13:07:29 +0000

    inventory.h keep working

---
+++ inventory.h
@@ -0,0 +1,22 @@
+/**
+ * @brief It defines a space
+ *
+ * @file inventory.h
+ * @author 
+ * @version 1.0
+ * @date 19-03-2018
+ * @copyright GNU Public License
+ */
+
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#include "types.h"
+#include "set.h"
+
+typedef struct _Inventory Inventory;
+
+Inventory * inventory_create();
+Status inentory_destroy(
+
+
