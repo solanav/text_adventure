7bab5b2ebd00cd2373e001b08f85b609dbc88b87
Guillermo Rios <guilleerioss@gmail.com>
2018-03-19 13:21:57 +0000

    inventory.c created online with Bitbucket

---
+++ inventory.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "types.h"
+#include "inventory.h"
+#include "set.h"
+
+struct _Inventory{
+Set * objects;
+int num_maxObj;
+};
+
+
+
+
