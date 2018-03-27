78ab2ee769a25b05e4b8c43da7124d9534b8872d
solanav <solanav00@gmail.com>
2018-03-27 15:58:47 +0200

    Graphic engine is now clean as a babys ass

--- graphic_engine.h
+++ graphic_engine.h
@@ -12,12 +12,22 @@
 #define __GRAPHIC_ENGINE__
 
 #include "game.h"
+#include "screen.h"
 
 typedef struct _Graphic_engine Graphic_engine;
 
 Graphic_engine* graphic_engine_create();
-void graphic_engine_destroy(Graphic_engine *ge);
-void graphic_engine_paint_game(Graphic_engine *ge, Game *game);
-void graphic_engine_write_command(Graphic_engine *ge, char *str);
+void graphic_engine_destroy(Graphic_engine *);
+void graphic_engine_paint_game(Graphic_engine *, Game *);
+
+/* 	
+	int = 0 -> up
+	int = 1 -> middle
+	int = 2 -> down
+*/
+void graphic_engine_paint_space(Graphic_engine *, Game *, int);
+
+void print_new_line(Area *, int number);
+char * create_objects_string(Game *, Id);
 
 #endif
