83ec6477a0897bf383be988237c589c2e9f9eb19
Guillermo Rios <guilleerioss@gmail.com>
2018-04-08 20:39:13 +0000

    die.h completado con @author @date, a peticion de cata

--- die.h
+++ die.h
@@ -10,20 +10,24 @@
     (donde se guarda el ultimo resultado de la funcion roll_die() ).
 */
 typedef struct _Die Die;
-/* 
+/*
+    @author
+    @date
     @brief esta funcion se encarga de crear el dado reservando memoria para el mismo.
     @param1 ID, el id del dado.
     @return newdie, el dado creado, o NULL si algo no ha salido como esperaba.
 */
 Die* die_ini(Id);
 void die_die_die(Die* );
-/*  
+/*  @author
+    @date
     @brief esta funcion se encarga de llamar a la funcion aleatorio() para simular la tirada del dado y devolver el numero elegido
     @return Die=>ult_tirada si ha salido correctamente o ERROR si no.
 */
 STATUS die_roll(Die* );
 short int die_get_last_roll(Die*);
-/*
+/*  @author
+    @date
     @brief esta funcion se encarga de imprimir el contenido del dado
     @param1 Die*die, el dado que desea imprimir.
     @return ERROR si algo no ha salido como se esperaba.
