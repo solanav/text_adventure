c08b06b77fd22cce1c754b50c197cc30c79c5183
Guillermo Rios <guilleerioss@gmail.com>
2018-04-08 20:07:03 +0000

    set.h casi comentado, faltan 4 funciones por comentar n se muy bien lo que hacen

--- set.h
+++ set.h
@@ -8,18 +8,36 @@
 #define MAX_INV_SIZE 1024
 
 typedef struct _Set Set;
-
+/*  @brief esta funcion se encarga de crear el conjunto reservando memoria para el mismo.
+    @return newset, el inventario creado, o NULL si algo no ha salido como esperaba.
+*/
 Set * set_create(int);
+/*  @brief esta funcion se encarga de destruir el conjunto.
+	@param1 el conjunto a destruir.
+   
+*/
 void set_destroy(Set *);
+/*  @brief esta funcion se encarga de añadir un elemento al conjunto.
+	@param1 el conjunto donde se desea añadir el elemento.
+	@param2 el id del elemento.
+   
+*/
 
 STATUS set_add(Set *, Id);
+/*  @brief esta funcion se encarga de añadir un elemento al conjunto.
+	@param1 el conjunto donde se desea añadir el elemento.
+	@param2 el id del elemento.
+    
+*/
 STATUS set_del(Set *, Id);
+/*  @brief esta funcion se encarga de obtener el id de un conjunto.
+	@param1 el conjunto del cual se desea obtener la id
+    @return devuelve el id del conjunto.
+*/
 Id set_get_id(Set *, int);
-
 STATUS set_rm_all(Set *);
 STATUS set_rearrange(Set *);
 Set * set_cp_all(Set *);
-
 STATUS set_print_debug(FILE *, Set *);
 
 #endif
